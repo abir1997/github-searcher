405ad3f46a7247450c5460916f5883c5fce85b16
abir1997 <s3677701@student.rmit.edu.au>
2019-09-22 20:25:00 +1000

    added local storage data

--- src/App.js
+++ src/App.js
@@ -3,28 +3,63 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Github from './Github';
 import Header from './Components/Header';
+import Auth0Lock from 'auth0-lock';
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
+
+    this.state = {
+      idToken: '',
+      profile: {}
+    };
   }
 
-  
 
   static defaultProps = {
     clientID: '27UIER4gWlruH76SVHc1Xts6Bwl7iUF6',
     domain: 'dev-sax926ea.au.auth0.com'
   }
 
-  componentDidMount(){
+  componentDidMount() {
+    this.lock = new Auth0Lock(this.props.clientID, this.props.domain);
 
+    this.lock.on('authenticated', (authResult) => {
+      console.log(authResult);
+
+      this.lock.getProfile(authResult.idToken, (err, profile) => {
+        if(err){
+          console.log(err);
+          return;
+        }
+       // console.log(profile);
+
+       this.setProfile(authResult.idToken, profile);
+
+      });
+    });
+  }
+
+  setProfile(idToken, profile){
+    localStorage.setItem('idToken', idToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+    
+    this.setState({
+      idToken : localStorage.getItem('idToken'),
+      profile : JSON.parse(localStorage.getItem('profile'))
+    });
+  }
+
+  showLock() {
+    this.lock.show();
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header
+          onLogin={this.showLock.bind(this)} />
         <Github />
       </div>
     );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
 
 class Header extends React.Component{
+
+    onLogin(){
+        this.props.onLogin();
+    }
+
     render(){
         return(
             <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">Github Searcher</Navbar.Brand>
             <Nav className="mr-auto">
               <Nav.Link href="#home"></Nav.Link>
-              <Nav.Link href="#features">Login</Nav.Link>
+              <Nav.Link onClick = {this.onLogin.bind(this)}>Login</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
             {/* <Form inline>
