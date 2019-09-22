4ec463f447ea86e05c4f22024f053b9a941bfb70
abir1997 <s3677701@student.rmit.edu.au>
2019-09-23 04:15:14 +1000

    gNight

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-class Search extends React.Component{
-    render(){
-        return(
-            <div className = "search-box">
-                <form>
+class Search extends React.Component {
+
+    submit(event) {
+        event.preventDefault();
+        let val = this.refs.username.value;
+        this.props.searchProfile(val);
+        this.refs.username.value = '';
+    }
+
+    render() {
+        return (
+            <div className="search-box">
+                <form onSubmit={this.submit.bind(this)}>
                     <label>
-                        <input type="search" ref="username" placeholder="type username"/>
+                        <input type="search" refs="username" placeholder="type username" />
                     </label>
                 </form>
             </div>
